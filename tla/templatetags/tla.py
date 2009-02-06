ae8f1d51580be5f422e193e4ea2a1aae057613e1
Bryan Veloso <bryan@revyver.com>
2009-02-05 23:08:48 -0800

    Tweaking the template and template tag. Removing the redundant get_xml() method. Still unsure if links will be returned as a dictionary.

--- tla/templatetags/tla.py
+++ tla/templatetags/tla.py
@@ -14,15 +14,22 @@ def tla_list(request):
     """
     url = 'http://www.text-link-ads.com/xml.php?inventory_key=' + settings.TLA_INVENTORY_KEY + '&referer=' + request.META.get('REQUEST_URI', request.META.get('PATH_INFO', '/'))
     agent = '&user_agent=' + request.META['HTTP_USER_AGENT']
-    links = get_xml(url+agent)
+    links = ElementTree.parse(urlopen(url+agent)).getroot()
     
     return {
         'links': links
     }
-
-def get_xml(url):
-    tree = ElementTree.parse(urlopen(url))
-    links = tree.getroot()
-    return links
+    
+    # This is here so I don't forget it. :P
+    # Don't know which way really works yet.
+    #
+    # return {
+    #     'links': {
+    #         'after_text': links.find('after_text').text,
+    #         'before_text': links.find('before_text').text,
+    #         'text': links.find('text').text,
+    #         'url': links.find('url').text,
+    #     }
+    # }
 
 register.inclusion_tag('templates/tla_list.html')(tla_list)
